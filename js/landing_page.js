4149db8fca3e24359be0920d329eca0a540be454
aaronjalapon <arquillanoaaron974@gmail.com>
2025-01-30 14:36:33 +0800

    landing_page_Update

--- js/landing_page.js
+++ js/landing_page.js
@@ -4,5 +4,6 @@ const navLinks = document.getElementById("navLinks");
 
 // Add an event listener for click events
 burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
   navLinks.classList.toggle("show"); // Toggle the 'show' class
 });
