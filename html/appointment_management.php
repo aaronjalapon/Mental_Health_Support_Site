9d936fab8c4a64f910b82ad5eb8fd4645c1519e8
aaronjalapon <arquillanoaaron974@gmail.com>
2025-02-25 10:18:46 +0800

    change the href on the appointment

--- html/appointment_management.php
+++ html/appointment_management.php
@@ -33,7 +33,7 @@ if(!isset($_SESSION['unique_id']) || $_SESSION['role'] !== 'admin') {
                 <li><a href="client_management.php"><i class="fas fa-users"></i> Clients</a></li>
                 <li><a href="therapist_management.php"><i class="fas fa-user-md"></i> Therapists</a></li>
                 <li><a href="appointment_management.php"><i class="fas fa-calendar-alt"></i> Appointments</a></li>
-                <li><a href="#"><i class="fas fa-comments"></i> Community</a></li>
+                <li><a href="community_management.php"><i class="fas fa-comments"></i> Community</a></li>
                 <li><a href="settings_management.php"><i class="fas fa-cog"></i> Settings</a></li>
                 <li><a href="#" onclick="handleLogout()"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
             </ul>
